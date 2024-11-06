b9ad2bedc2867acbe7f1f5178492d90f69a81494
Igor Chaporgin <fellclub@yandex.ru>
2024-11-06 18:43:45 +0300

    KS-2-1 create variable average score

---
+++ src/main/kotlin/lesson_2/Lesson2_task1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+    val averageScore: Float = (student1.toFloat() + student2.toFloat() + student3.toFloat()
+            + student4.toFloat()) / 4.0f
+
+    println("Average score: $averageScore")
+}
