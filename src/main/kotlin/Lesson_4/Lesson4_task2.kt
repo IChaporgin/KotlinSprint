645d136b19f11119d41492a0b772ff818bd238f2
Igor Chaporgin <fellclub@yandex.ru>
2024-11-08 17:03:42 +0300

    KS-4-2 create customs check

--- src/main/kotlin/Lesson_4/Lesson4_task2.kt
+++ src/main/kotlin/Lesson_4/Lesson4_task2.kt
@@ -16,6 +16,7 @@ fun main() {
     val volumeCargo1 = 80
     val weightCargo2 = 50
     val volumeCargo2 = 100
+
     println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории \"AVERAGE\": ${(weightCargo1 > MIN_WEIGHT && weightCargo1 <= MAX_WEIGHT) && volumeCargo1  <= MAX_VOLUME}")
     println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории \"AVERAGE\": ${(weightCargo2 > MIN_WEIGHT && weightCargo2 <= MAX_WEIGHT) && volumeCargo2  <= MAX_VOLUME}")
 
