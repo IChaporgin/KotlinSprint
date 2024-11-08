eb873574a4e3d04d83f10357fa1a269ea4517db1
Igor Chaporgin <fellclub@yandex.ru>
2024-11-08 09:51:31 +0300

    KS-2-4 fixed solution

--- src/main/kotlin/lesson_2/Lesson2_task4.kt
+++ src/main/kotlin/lesson_2/Lesson2_task4.kt
@@ -1,13 +1,14 @@
 fun main() {
-    val baff = 0.2f
+    val baff = 20
+    val percent = 100
     val cristallOre = 7
     val ironOre = 11
 
-    val cristallOreBaff = (cristallOre * baff).toInt()
-    val ironOreBaff = (ironOre * baff).toInt()
+    val cristallOreBaff = cristallOre * baff / percent
+    val ironOreBaff = ironOre * baff / percent
 
-    println("Бафф составляет ${(baff * 100).toInt()}% Вы получили $cristallOreBaff единицы кристаллической руды!")
-    println("Бафф составляет ${(baff * 100).toInt()}% Вы получили $ironOreBaff единицы железной руды!")
+    println("Бафф составляет ${baff}% Вы получили $cristallOreBaff единицы кристаллической руды!")
+    println("Бафф составляет ${baff}% Вы получили $ironOreBaff единицы железной руды!")
 
 
 }
