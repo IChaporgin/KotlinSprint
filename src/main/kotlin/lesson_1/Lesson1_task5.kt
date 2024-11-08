6ee4d018160bbab300b1e2dc8ed678e6bac60ac7
Igor Chaporgin <fellclub@yandex.ru>
2024-11-08 10:12:55 +0300

    KS-1-5 fixed solution and create new constants

--- src/main/kotlin/lesson_1/Lesson1_task5.kt
+++ src/main/kotlin/lesson_1/Lesson1_task5.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_1
 
 fun main() {
-    val totalSeondOfFloght = 6480
-    val secondInHour = 3600
-    val minuteInHour = 60
-    val hourOfFlight = totalSeondOfFloght / secondInHour
-    val reamningSecond = totalSeondOfFloght % secondInHour
-    val minuteOfFlight = reamningSecond / minuteInHour
-    val secondOfFlight = reamningSecond % minuteInHour
+    val totalSecondOfFlight = 6480
+
+    val hourOfFlight = totalSecondOfFlight / SECOND_IN_HOUR
+    val remaningSecond = totalSecondOfFlight % SECOND_IN_HOUR
+    val minuteOfFlight = remaningSecond / MINUTE_IN_HOUR
+    val secondOfFlight = remaningSecond % MINUTE_IN_HOUR
     println("Time of flight: ${"%02d".format(hourOfFlight)}:${"%02d".format(minuteOfFlight)}:${"%02d".format(secondOfFlight)}")
 }
+const val SECOND_IN_HOUR = 3600
+const val MINUTE_IN_HOUR = 60
