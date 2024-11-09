4dd8e07b836afe2f004aeff2682cbc1dc3008050
Igor Chaporgin <fellclub@yandex.ru>
2024-11-09 15:54:05 +0300

    KS-4-2 added line splitting

--- src/main/kotlin/Lesson_4/Lesson4_task2.kt
+++ src/main/kotlin/Lesson_4/Lesson4_task2.kt
@@ -17,8 +17,12 @@ fun main() {
     val weightCargo2 = 50
     val volumeCargo2 = 100
 
-    println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории \"AVERAGE\": ${(weightCargo1 > MIN_WEIGHT && weightCargo1 <= MAX_WEIGHT) && volumeCargo1  <= MAX_VOLUME}")
-    println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории \"AVERAGE\": ${(weightCargo2 > MIN_WEIGHT && weightCargo2 <= MAX_WEIGHT) && volumeCargo2  <= MAX_VOLUME}")
+    println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории" +
+            " \"AVERAGE\": ${(weightCargo1 > MIN_WEIGHT && weightCargo1 <= MAX_WEIGHT) && 
+                    volumeCargo1  <= MAX_VOLUME}")
+    println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории " +
+            "\"AVERAGE\": ${(weightCargo2 > MIN_WEIGHT && weightCargo2 <= MAX_WEIGHT) && 
+                    volumeCargo2  <= MAX_VOLUME}")
 
 }
 const val MIN_WEIGHT = 35
