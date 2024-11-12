351ce57a79c6bc5ea231516c27665e4bae88f3e9
Igor Chaporgin <fellclub@yandex.ru>
2024-11-12 15:35:26 +0300

    KS-3-1 modify greeting

--- src/main/kotlin/Lesson_3/Lesson3_task1.kt
+++ src/main/kotlin/Lesson_3/Lesson3_task1.kt
@@ -2,8 +2,5 @@ fun main() {
     val name = readln()
     val morningGreeting = "Доброе утро! "
     val eveningGreeting = "Добрый вечер! "
-    var greeting = morningGreeting + name
-    println(greeting)
-    greeting = eveningGreeting + name
-    println(greeting)
+    println("$morningGreeting, $name! Рады Вас видеть!")
 }
