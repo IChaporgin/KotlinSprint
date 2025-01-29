37668568a6ac9af6f404607da9ad6b3e06df6770
Igor Chaporgin <fellclub@yandex.ru>
2025-01-29 20:53:11 +0300

    KS-8-4 create ingredients substitution

---
+++ src/main/kotlin/Lesson_8/Lesson8_task4.kt
@@ -0,0 +1,24 @@
+package org.example.Lesson_8
+
+fun main() {
+    val ingredients = mutableListOf("молоко", "кефир", "сахар", "яблоки", "соль", "мука")
+
+    for (i in ingredients){
+        println("${ingredients.indexOf(i) + 1}. Ингредиент: $i")
+    }
+    println("Какой ингредиент вы хотите заменить?")
+    val product = readln()
+
+    if (product !in ingredients){
+        println("Такого ингредиента нет!")
+        return
+    } else {
+        println("Какой ингредиент добавить?")
+        val addIngredient = readln()
+        ingredients[ingredients.indexOf(product)] = addIngredient
+    }
+    println("Готово! Вы сохранили следующий список:")
+    for (i in ingredients) {
+        println("${ingredients.indexOf(i) + 1}. Ингредиент: $i")
+    }
+}
