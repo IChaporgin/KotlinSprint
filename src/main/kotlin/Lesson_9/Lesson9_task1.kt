dd8489533100df26feae34bb1b99c48b42b1d09b
Igor Chaporgin <fellclub@yandex.ru>
2025-01-30 12:52:26 +0300

    KS-9-1 create ingredients list

---
+++ src/main/kotlin/Lesson_9/Lesson9_task1.kt
@@ -0,0 +1,7 @@
+package org.example.Lesson_9
+
+fun main() {
+    val ingredientsList = listOf("молоко", "кефир", "сахар", "яблоки", "соль", "мука")
+    println("В рецепте есть следующие ингредиенты:")
+    ingredientsList.forEach { println(it) }
+    }
