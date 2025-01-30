6f10958eafaaea15e70e390d56ac8e4cab383a46
Igor Chaporgin <fellclub@yandex.ru>
2025-01-30 22:55:01 +0300

    KS-7-5 modify solution and create new value

--- src/main/kotlin/Lesson_7/Lesson7_task5.kt
+++ src/main/kotlin/Lesson_7/Lesson7_task5.kt
@@ -2,26 +2,17 @@ package org.example.Lesson_7
 
 fun main() {
 
-    val capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    val lowLetters = "abcdefghijklmnopqrstuvwxyz"
-    val numbers = "0123456789"
-    val symbols = "@$!"
+    val allSymbols = ('A'..'Z') + ('a'..'z') + ('0'..'9') + ('!'..'@')
     var number: Int
+    var password = ""
 
     do {
         println("Введите длину пароля, минимум 6 символов:")
         number = readln().toInt()
     } while (number < 6)
 
-    var password = ""
-
     for (i in 0..number) {
-        when (i % 4) {
-            0 -> password += capitalLetters.random()
-            1 -> password += lowLetters.random()
-            2 -> password += numbers.random()
-            3 -> password += symbols.random()
-        }
+        password += allSymbols.random()
     }
     println("Ваш пароль: ${password}")
 }
