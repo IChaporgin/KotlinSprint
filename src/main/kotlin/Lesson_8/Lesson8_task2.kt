2977a99c6c2adaf89273c301bc4f2e20223d7ad5
Igor Chaporgin <fellclub@yandex.ru>
2025-01-30 23:02:02 +0300

    KS-8-2 modify solution

--- src/main/kotlin/Lesson_8/Lesson8_task2.kt
+++ src/main/kotlin/Lesson_8/Lesson8_task2.kt
@@ -2,22 +2,12 @@ package org.example.Lesson_8
 
 fun main() {
     val ingredientApplePie = arrayOf("яйца", "сметана", "мука", "сахар", "яблоки")
-    var result = false
     println("Какой ингредиент вы хотите найти?")
     val ingredient = readln()
-    for (i in ingredientApplePie) {
-        if (i == ingredient) {
-            result = true
-            break
-        }
-    }
 
-    if (result){
+    if (ingredient in ingredientApplePie){
         println("Ингредиент $ingredient в рецепте есть")
     } else {
         println("Такого ингредиента в рецепте нет")
     }
-
-
-
 }
