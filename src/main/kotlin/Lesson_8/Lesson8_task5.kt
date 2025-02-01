835b99387f722fcc4cd10e5a85b753d8034cbe44
Igor Chaporgin <fellclub@yandex.ru>
2025-02-01 16:01:28 +0300

    KS-8-5 modify solution

--- src/main/kotlin/Lesson_8/Lesson8_task5.kt
+++ src/main/kotlin/Lesson_8/Lesson8_task5.kt
@@ -2,12 +2,8 @@ package org.example.Lesson_8
 
 fun main() {
     println("Введите количество ингредиентов, которые вы хотите добавить:")
-    val count = readln().toInt()
-    val ingredients = Array<String>(count) {""}
-    for (i in 1..count) {
-        println("Введите $i ингредиент:")
-        val ingredient = readln()
-        ingredients[i - 1] = ingredient
-    }
+//    val count = readln().toInt()
+    val ingredients = Array<String>(readln().toInt()) { readln() }
+
     println("Ваши ингредиенты: ${ingredients.joinToString(", ")}")
 }
