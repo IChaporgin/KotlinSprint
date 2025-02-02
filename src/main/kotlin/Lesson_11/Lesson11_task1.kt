b89bd12cb7ee395a89c1b7ca5f5ea7d20053562d
Igor Chaporgin <fellclub@yandex.ru>
2025-02-02 12:53:08 +0300

    KS-11-1 create class user

---
+++ src/main/kotlin/Lesson_11/Lesson11_task1.kt
@@ -0,0 +1,28 @@
+package org.example.Lesson_11
+
+fun main() {
+    val user1 = User(
+        id = 1,
+        login = "Bob",
+        password = "12345",
+        email = "bob@bob.com"
+    )
+
+    val user2 = User(
+        id = 2,
+        login = "Jhon",
+        password = "qwert",
+        email = "jhon@jhon.com"
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.email)
+    println()
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.email)
+
+}
