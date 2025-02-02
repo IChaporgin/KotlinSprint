f1d804653ab7cda1159251aeb392fa8af7f4b614
Igor Chaporgin <fellclub@yandex.ru>
2025-02-02 10:54:48 +0300

    KS-9-1 code formatting

--- src/main/kotlin/Lesson_9/Lesson9_task1.kt
+++ src/main/kotlin/Lesson_9/Lesson9_task1.kt
@@ -4,4 +4,4 @@ fun main() {
     val ingredientsList = listOf("молоко", "кефир", "сахар", "яблоки", "соль", "мука")
     println("В рецепте есть следующие ингредиенты:")
     ingredientsList.forEach { println(it) }
-    }
+}
