35484e9a5f023c834c5da4eae9afd18f34547c88
Igor Chaporgin <fellclub@yandex.ru>
2025-02-03 10:58:58 +0300

    KS-9-5 modify solution add new set

--- src/main/kotlin/Lesson_9/Lesson9_task5.kt
+++ src/main/kotlin/Lesson_9/Lesson9_task5.kt
@@ -1,17 +1,14 @@
 package org.example.Lesson_9
 
 fun main() {
-    val products: MutableList<String> = mutableListOf()
+    val products: MutableSet<String> = mutableSetOf()
     println("Введите список продуктов:")
     while (products.count() < 5) {
         val product = readln()
-        if (product !in products) {
-            products.add(product)
-            products.sort()
-        } else {
+        if (!products.add(product)) {
             println("Такой продукт уже есть в списке!")
         }
     }
-    val modifyProductsList = products.map { it.replaceFirstChar { it.uppercase() } }
-    println(modifyProductsList.joinToString(", "))
+    val sortedProducts = products.map { it.replaceFirstChar { it.uppercase() } }.sorted()
+    println(sortedProducts.joinToString(", "))
 }
