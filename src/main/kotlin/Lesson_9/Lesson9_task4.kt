738045a80013bc32861c582e982e43285e1852ed
Igor Chaporgin <fellclub@yandex.ru>
2025-02-03 09:45:54 +0300

    KS-9-4 modify solution add new list to sort products

--- src/main/kotlin/Lesson_9/Lesson9_task4.kt
+++ src/main/kotlin/Lesson_9/Lesson9_task4.kt
@@ -3,8 +3,6 @@ package org.example.Lesson_9
 fun main() {
     println("Напиши пять ингредиентов через запятую:")
     val inputProducts = readln().split(", ")
-//    val productsList: MutableList<String> = mutableListOf()
-//    productsList.addAll(inputProducts)
     val sortProducts = inputProducts.sorted()
     println("Список продуктов:")
     sortProducts.forEach { println(it) }
