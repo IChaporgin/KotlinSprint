69d5aa9fd17960ea0bb0b43533352e20211b5f9d
Igor Chaporgin <fellclub@yandex.ru>
2025-02-03 14:32:58 +0300

    KS-11-2 created new class user with new method

---
+++ src/main/kotlin/Lesson_11_22/User2.kt
@@ -0,0 +1,34 @@
+package org.example.Lesson_11_22
+
+class User2 (
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String = "",
+){
+
+    fun getInformation(){
+        println("ID: $id\nLogin: $login\nPassword: $password\nEmail: $email\nBio:$bio")
+    }
+
+    fun getBio(){
+        println("Информация: $bio\nХотите изменить?")
+        if (readln().lowercase() == "да"){
+            println("Введите новую информацию:")
+            this.bio = readln()
+            println("Информация обновлена!")
+        }
+    }
+
+    fun changePassword(){
+        println("Введите старый пароль:")
+        if (readln() == password){
+            println("Введите новый пароль:")
+            this.password = readln()
+            println("Пароль обновлен!")
+        } else {
+            println("Неверный пароль")
+        }
+    }
+}
