3d831c67296c8deb10ceed5f7ba11388f0284632
Igor Chaporgin <fellclub@yandex.ru>
2025-02-04 17:42:36 +0300

    KS-12-3 created class weather to convert Kelvin to Celsius

---
+++ src/main/kotlin/Lesson_11_22/Lesson12_task3.kt
@@ -0,0 +1,10 @@
+package org.example.Lesson_11_22
+
+fun main() {
+    val weather = WeatherCelsius(
+        86,
+        54,
+        true
+    )
+    weather.showWeather()
+}
---
+++ src/main/kotlin/Lesson_11_22/WeatherCelsius.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_11_22
+
+class WeatherCelsius(val dayTemperature: Int, val nightTemperature: Int, val precipitation: Boolean) {
+
+    val dayTemperatureCelsius: Int
+    val nightTemperatureCelsius: Int
+
+    init {
+        dayTemperatureCelsius = dayTemperature - 273
+        nightTemperatureCelsius = nightTemperature - 273
+    }
+
+    fun showWeather(){
+        println("Дневная температура: $dayTemperatureCelsius°C\nНочная температура: $nightTemperatureCelsius°C\nОсадки: $precipitation")
+    }
+}
