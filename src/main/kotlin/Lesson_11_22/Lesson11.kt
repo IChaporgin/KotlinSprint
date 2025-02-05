26b98918642b98935b2bca9e907bd55d5f0c5c35
Igor Chaporgin <fellclub@yandex.ru>
2025-02-05 10:58:15 +0300

    KS-11-2 modified solution added code to one file

--- src/main/kotlin/Lesson_11_22/Lesson11.kt
+++ src/main/kotlin/Lesson_11_22/Lesson11.kt
@@ -1,5 +1,20 @@
 package org.example.Lesson_11_22
 
+fun main() {
+
+    val user = User2(
+        id = 2,
+        login = "Bob",
+        password = "123",
+        email = "bob@bob.com",
+    )
+
+    user.getInformation()
+    user.getBio()
+    user.changePassword()
+    user.getInformation()
+}
+
 class User2 (
     val id: Int,
     val login: String,
