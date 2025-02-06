9331ce5117143ae1de0d1fc9e19ae312ffeb69df
Igor Chaporgin <fellclub@yandex.ru>
2025-02-06 11:22:52 +0300

    KS-12-1 modify solution

--- src/main/kotlin/Lesson_11_22/Lesson12_task1.kt
+++ src/main/kotlin/Lesson_11_22/Lesson12_task1.kt
@@ -17,3 +17,13 @@ fun main() {
     println()
     weather2.showWeather()
 }
+
+class Weather() {
+    var dayTemperature: Double = 25.3
+    var nightTemperature: Double = 15.1
+    var precipitation: Boolean = false
+
+    fun showWeather(){
+        println("Дневная температура: $dayTemperature\nНочная температура: $nightTemperature\nОсадки: $precipitation")
+    }
+}
