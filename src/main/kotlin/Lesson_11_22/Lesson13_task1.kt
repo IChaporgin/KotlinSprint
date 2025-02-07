3a60ca3d110e29557ad58af865a3b1e8f334b8a5
Igor Chaporgin <fellclub@yandex.ru>
2025-02-07 10:00:38 +0300

    KS-13-1 modify solution

--- src/main/kotlin/Lesson_11_22/Lesson13_task1.kt
+++ src/main/kotlin/Lesson_11_22/Lesson13_task1.kt
@@ -1,11 +1,16 @@
 package org.example.Lesson_11_22
 
 fun main() {
+    val name = PhoneBook(
+        "asd",
+        1231313121,
+        null
+    )
 
 }
 
 class PhoneBook(
     val name: String,
     val number: Long,
-    val company: String? = null
+    val company: String?
 )
--- src/main/kotlin/Lesson_1_10/Lesson_10/Lesson10_task1.kt
+++ src/main/kotlin/Lesson_1_10/Lesson_10/Lesson10_task1.kt
@@ -3,10 +3,10 @@ package org.example.Lesson_1_10.Lesson_10
 fun main() {
     println("Предлагаю сыграть в игру кости!")
     do {
-        val progressPlayer = getNumber()
+        val progressPlayer = getNumber1()
         println("У вас выпало: $progressPlayer")
         Thread.sleep(1000)
-        val progressComputer = getNumber()
+        val progressComputer = getNumber1()
         println("У машины выпало: $progressComputer")
         Thread.sleep(1000)
 
@@ -23,7 +23,7 @@ fun main() {
     } while (answer.lowercase() == "да")
 }
 
-fun getNumber() : Int {
+fun getNumber1() : Int {
     val range = 1..6
     return range.random()
 }
