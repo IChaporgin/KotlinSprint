8b4a7d2593119d35ea747a24e7c80ab3efdb24df
Igor Chaporgin <fellclub@yandex.ru>
2025-02-19 09:29:49 +0300

    KS-19-2 created class EcommerceProducts and enum class Products with method showDescription

---
+++ src/main/kotlin/Lesson_11_22/Lesson_19/Lesson19_task2.kt
@@ -0,0 +1,41 @@
+package org.example.Lesson_11_22.Lesson_19
+
+fun main() {
+    val listProducts = listOf(
+        EcommerceProducts(1, Products.CLOTHING),
+        EcommerceProducts(2, Products.CLOTHING),
+        EcommerceProducts(3, Products.STATIONARY)
+    )
+
+    listProducts.forEach { println(it.showInfo()) }
+    listProducts.forEach { println(it.product.getDescription()) }
+}
+
+class EcommerceProducts(
+    val id: Int,
+    val product: Products
+) {
+    fun showInfo() : String {
+        return "Товар id: $id тип: ${product.getDescription()}"
+    }
+}
+
+enum class Products {
+    CLOTHING {
+        override fun getDescription(): String {
+            return "Одежда"
+        }
+    },
+    STATIONARY {
+        override fun getDescription(): String {
+            return "Канцелярские товары"
+        }
+    },
+    MISCELLANEOUS {
+        override fun getDescription(): String {
+            return "Разное"
+        }
+    };
+
+    abstract fun getDescription() : String
+}
