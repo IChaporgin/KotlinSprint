2f62f448001c3a609b4fb4ce3a9c3c62577c0c1e
Igor Chaporgin <fellclub@yandex.ru>
2025-02-20 21:20:39 +0300

    KS-20-4 created lambda function even element

--- src/main/kotlin/Lesson_11_22/Lesson_20/Lesson20_task4.kt
+++ src/main/kotlin/Lesson_11_22/Lesson_20/Lesson20_task4.kt
@@ -3,9 +3,9 @@ package org.example.Lesson_11_22.Lesson_20
 fun main() {
     val listElements = listOf("Круг", "Квадрат", "Треугольник", "Овал")
     val elements = listElements.map { element -> { println("Нажат: $element") }}
-    elements.forEachIndexed { index, action ->
+    elements.forEachIndexed { index, value ->
         if (index % 2 == 1) {
-            action()
+            value()
         }
     }
 }
