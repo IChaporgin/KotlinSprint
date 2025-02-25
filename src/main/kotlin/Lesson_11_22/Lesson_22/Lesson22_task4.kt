9f5e977ac0b5d1c3b9160d8793790e837a7656c8
Igor Chaporgin <fellclub@yandex.ru>
2025-02-25 22:58:47 +0300

    KS-22-4 modify solution

--- src/main/kotlin/Lesson_11_22/Lesson_22/Lesson22_task4.kt
+++ src/main/kotlin/Lesson_11_22/Lesson_22/Lesson22_task4.kt
@@ -5,16 +5,18 @@ fun main() {
     view.loadData()
 }
 
-data class MainScreenState(
-    val data: String = "",
-    val isLoading: Boolean = false
-)
-
 class MainScreenViewModel {
-    var mainScreenState = MainScreenState()
+
+    data class MainScreenState(
+        val data: String = "",
+        val isLoading: Boolean = false
+    )
+
+    private var mainScreenState = MainScreenState()
+
     fun loadData() {
         println("- отсутствие данных")
-       Thread.sleep(1000)
+        Thread.sleep(1000)
         mainScreenState = mainScreenState.copy(data = "- загрузка данных", isLoading = true)
         println(mainScreenState.data)
         Thread.sleep(1000)
